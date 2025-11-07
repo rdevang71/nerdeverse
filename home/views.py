b8d2e64fad29e6b95215242ca56bb0fd4b6fe438
rdevang71 <rdevang71@gmail.com>
2025-11-07 19:13:40 +0530

    comp[leted rendering on home page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
+# home/views.py
 from django.shortcuts import render
+from wishlists.models import WishlistItem  # import your model
 
 def homepage(request):
-    return render(request, 'index.html')
+    all_items = WishlistItem.objects.select_related('user', 'category').order_by('-created_at')
+    return render(request, 'index.html', {'wishlist_items': all_items})
